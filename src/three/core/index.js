fcce0980628e6dd65112b61014f4f7d44a376018
Randy Little <randykeithlittle@github.com>
2018-09-05 11:49:53 -0500

    ADD build

--- src/three/core/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Component } from "./component";
-export { default as Loop } from "./loop";
-export { default as Entity } from "./entity";
-export { default as Collection } from "./collection";
--- src/ui/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Renderer } from "./renderer";
-export { default as File } from "./file";
-export { default as Number } from "./number";
-export { default as XByY } from "./xbyy";
-export { default as View } from "./view";
-export { default as Rotate } from "./rotate";
-export { default as Tile } from "./tile";
